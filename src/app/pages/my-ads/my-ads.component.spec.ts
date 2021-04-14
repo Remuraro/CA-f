004749e182660d95d3835891f0540fc226792fdb
Remuraro <79248198+Remuraro@users.noreply.github.com>
2021-04-14 01:17:57 +0100

    testing

--- src/app/pages/my-ads/my-ads.component.spec.ts
+++ src/app/pages/my-ads/my-ads.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MyAdsComponent } from './my-ads.component';
+import { AppComponent } from '../../app.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AdvertService } from 'src/app/services/advert-service/advert.service';
+import { ApiService } from 'src/app/services/api-service/api.service';
+
 
 describe('MyAdsComponent', () => {
   let component: MyAdsComponent;
@@ -8,7 +13,9 @@ describe('MyAdsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MyAdsComponent ]
+      declarations: [ MyAdsComponent ],
+      providers: [AppComponent, AdvertService, ApiService],
+      imports: [FormsModule, AppRoutingModule]
     })
     .compileComponents();
   });
